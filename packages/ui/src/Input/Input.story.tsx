87e4541364bec10e8b3d0fafbee322a39aac59aa
Ephrem B <83043333+marvel-eb@users.noreply.github.com>
2022-03-02 15:09:39 +0100

    EmailValidation
    Latest Update:
    
    //WORKS FINE BY USING JS EXTENTION
    I used JS extension and works fine /web/src/domain/auth/pages/LogIn.js
    
    //PROBLEM HAS OCCURED bY USING TYPESCRIPT EXTENSION
    Ui/src//input/input.story.tsx

--- packages/ui/src/Input/Input.story.tsx
+++ packages/ui/src/Input/Input.story.tsx
@@ -1,20 +1,63 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import { within, userEvent } from '@storybook/testing-library'
 import { Story, Meta, StoryObj } from '@storybook/react'
-
 import { Input, InputProps, INPUT_SELECTOR } from './Input'
+
+////////////////////////////////////////////////////
+// Import Styleshee // EPHREM
+import "../../../Styles/main.css";
 
 type StoryType = StoryObj<InputProps>
 
+// Component 01
 const Template: Story<InputProps> = (args) => {
-  const [state, setState] = useState(args.value || '')
+const [state, setState] = useState(args.value || '')
 
-  return <Input {...args} value={state} onChange={(value, e) => {
+////////////////////////////////////////////////////
+// Let us handle Success / Error Messages // EPHREM
+const [isValid, setIsValid] = useState(false);
+const [message, setMessage] = useState("");
+
+// Email pattern to catch  errors 
+const result =
+/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const validateEmail = (e) => {
+const email = e.target.value;
+if (result.test(email)) {
+  setIsValid(true);
+  setMessage("Awesome, Your Email Address looks good.");
+} else {
+  setIsValid(false);
+  setMessage("Sorry, Please Enter a valid Email Address.");
+}
+};
+
+  return (
+  
+  <Input {...args} value={state} onChange={(value, e) => {
     setState(value)
     args.onChange && args.onChange(value, e)
   }} />
+
+  <Input
+  type="email"
+  placeholder="Enter your email"
+  onChange={validateEmail}
+/>
+
+{/* STYLES = Success / Error Messages Goes Here */}
+
+<div className={`message ${isValid ? "successMessage" : "errorMessage"}`}>
+{message}
+</div>
+
+  
+  );
 }
 
+
+// Component 02
 export default {
   title: 'Components/Input',
   component: Template.bind({}),
@@ -30,6 +73,12 @@ export const Default: StoryType = {
   }
 }
 
+<<<<<<< Updated upstream
+=======
+
+
+// Component 03
+>>>>>>> Stashed changes
 export const Controlled: StoryType = {
   name: 'Controlled',
   args: {
---
+++ packages/web/src/domains/auth/pages/Login.js
@@ -0,0 +1,65 @@
+import React, { Component, useState } from "react";
+
+////////////////////////////////////////////////////
+// Import Styleshee // EPHREM
+import "../../../Styles/main.css";
+
+
+import styled from "styled-components";
+import { Panel, Input } from "@zealous/ui";
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color: #f4f4f4;
+`;
+///////////////////////////////////////// EPHREM
+// Functional Component (arrow Function)
+// const LoginPage = () => {
+  //  function  LoginPage (){
+  export function LoginPage() {
+
+  const [isValid, setIsValid] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const result =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const validateEmail = (e) => {
+    const email = e.target.value;
+    if (result.test(email)) {
+      setIsValid(true);
+      setMessage("Awesome, Your Email Address looks good.");
+    } else {
+      setIsValid(false);
+      setMessage("Sorry, Please Enter a valid Email Address.");
+    }
+  };
+
+  return (
+    <Layout>
+      <Panel>
+        <Input placeholder="Your Email Address" />
+
+        <input
+        className="emailInput"
+          type="email"
+          placeholder="Enter your email"
+          onChange={validateEmail}
+        />
+
+        {/* Success / Error Messages Goes Here */}
+
+        <div className={`message ${isValid ? "successMessage" : "errorMessage"}`}>
+          {message}
+        </div>
+        
+      </Panel>
+    </Layout>
+  );
+};
+
+// export default LoginPage;

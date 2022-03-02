94e959bd5b2210754e84c404d19a41ffe9469ade
Ephrem B <83043333+marvel-eb@users.noreply.github.com>
2022-03-02 18:59:44 +0100

    Update Input.story.tsx

--- packages/ui/src/Input/Input.story.tsx
+++ packages/ui/src/Input/Input.story.tsx
@@ -1,98 +1,89 @@
-import { Component, useState } from 'react'
-import { within, userEvent } from '@storybook/testing-library'
-import { Story, Meta, StoryObj } from '@storybook/react'
-import { Input, InputProps, INPUT_SELECTOR } from './Input'
+import { Component, useState } from "react";
+import { within, userEvent } from "@storybook/testing-library";
+import { Story, Meta, StoryObj } from "@storybook/react";
+import { Input, InputProps, INPUT_SELECTOR } from "./Input";
 
 ////////////////////////////////////////////////////
 // Import Styleshee // EPHREM
 import "../../../web/src/Styles/main.css";
 
-type StoryType = StoryObj<InputProps>
+type StoryType = StoryObj<InputProps>;
 
 // Module 01
 const Template: Story<InputProps> = (args) => {
-const [state, setState] = useState(args.value || '')
+  const [state, setState] = useState(args.value || "");
 
-////////////////////////////////////////////////////
-// Let us handle Success / Error Messages // EPHREM
-const [isValid, setIsValid] = useState(false);
-const [message, setMessage] = useState("");
+  ////////////////////////////////////////////////////
+  // Let us handle Success / Error Messages // EPHREM
+  const [isValid, setIsValid] = useState(false);
+  const [message, setMessage] = useState("");
 
-// Email pattern to catch  errors 
-const result =
-/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // Email pattern to catch  errors
+  const result =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const validateEmail = (e) => {
-const email = e.target.value;
-if (result.test(email)) {
-  setIsValid(true);
-  setMessage("Awesome, Your Email Address looks good.");
-} else {
-  setIsValid(false);
-  setMessage("Sorry, Please Enter a valid Email Address.");
-}
-};
+  const validateEmail = (e) => {
+    const email = e.target.value;
+    if (result.test(email)) {
+      setIsValid(true);
+      setMessage("Awesome, Your Email Address looks good.");
+    } else {
+      setIsValid(false);
+      setMessage("Sorry, Please Enter a valid Email Address.");
+    }
+  };
 
   return (
-  <>
-  <Input {...args} value={state} onChange={ (value, e) => {
-    setState(value)
- 
-  args.onChange && args.onChange(value,  e) 
- }} />
+    <>
+      <Input
+        {...args}
+        value={state}
+        onChange={(value, e) => {
+          setState(value);
 
+          args.onChange && args.onChange(value, e);
+        }}
+      />
 
-  {/* <Input
-  // type="email"
-  placeholder="Enter your email"
-  onChange={validateEmail}
-/> */}
+      <Input placeholder="Enter your email" onChange={validateEmail} />
 
+      {/* //  STYLES = Success / Error Messages Goes Here  */}
 
-
-{/* //  STYLES = Success / Error Messages Goes Here  */}
-
-<div className={`message ${isValid ? "successMessage" : "errorMessage"}`}>
-{message}
-</div>
- </>
-  
+      <div className={`message ${isValid ? "successMessage" : "errorMessage"}`}>
+        {message}
+      </div>
+    </>
   );
 };
 
-
-
 // Module 02
 export default {
-  title: 'Components/Input',
+  title: "Components/Input",
   component: Template.bind({}),
   args: {
-    value: 'this is a value'
-  }
-} as Meta
-
+    value: "this is a value",
+  },
+} as Meta;
 
 // Module 03
 export const Default: StoryType = {
-  name: 'Email',
+  name: "Email",
   args: {
-    value: 'Input'
-  }
-}
-
-
+    value: "Input",
+  },
+};
 
 // Module 04
 export const Controlled: StoryType = {
-  name: 'Controlled',
+  name: "Controlled",
   args: {
-    value: ''
+    value: "",
   },
   play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
+    const canvas = within(canvasElement);
     await userEvent.type(
       await canvas.findByTestId(INPUT_SELECTOR),
-      'Hello World'
-    )
-  }
-}
+      "Hello World"
+    );
+  },
+};
